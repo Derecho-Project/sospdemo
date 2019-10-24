0392aca9a1f8d1c21aed9c1d91d52162ed3f44bd
Matthew Milano <milano@cs.cornell.edu>
2019-10-24 19:47:08 -0400

    merge conflict gone

--- src/derecho-component/server_logic.cpp
+++ src/derecho-component/server_logic.cpp
@@ -11,63 +11,23 @@
 #include <mxnet-component/utils.hpp>
 #include <vector>
 
-#define CONF_SOSPDEMO_FUNCTION_TIER_MIN_NODES "SOSPDEMO/function_tier_min_nodes"
-#define CONF_SOSPDEMO_FUNCTION_TIER_MAX_NODES "SOSPDEMO/function_tier_max_nodes"
-#define CONF_SOSPDEMO_CATEGORIZER_TIER_NUM_SHARD                               \
-  "SOSPDEMO/categorizer_tier_num_shard"
-#define CONF_SOSPDEMO_CATEGORIZER_TIER_MIN_NODES                               \
-  "SOSPDEMO/categorizer_tier_min_nodes"
-#define CONF_SOSPDEMO_CATEGORIZER_TIER_MAX_NODES                               \
-  "SOSPDEMO/categorizer_tier_max_nodes"
-
 /**
  * Start a server node
  */
-void do_server(int argc, char **argv) {
+void do_server(int argc, char **argv)
+{
   // load configuration
   derecho::Conf::initialize(argc, argv);
 
   // 1 - create subgroup info using the default subgroup allocator function
   // Both the function tier and the categorizer tier subgroups have one shard,
   // with two members in each shard,respectively.
-  uint32_t function_tier_min_nodes =
-      derecho::getConfUInt32(CONF_SOSPDEMO_FUNCTION_TIER_MIN_NODES);
-  uint32_t function_tier_max_nodes =
-      derecho::getConfUInt32(CONF_SOSPDEMO_FUNCTION_TIER_MAX_NODES);
-  uint32_t categorizer_tier_num_shard =
-      derecho::getConfUInt32(CONF_SOSPDEMO_CATEGORIZER_TIER_NUM_SHARD);
-  uint32_t categorizer_tier_min_nodes =
-      derecho::getConfUInt32(CONF_SOSPDEMO_CATEGORIZER_TIER_MIN_NODES);
-  uint32_t categorizer_tier_max_nodes =
-      derecho::getConfUInt32(CONF_SOSPDEMO_CATEGORIZER_TIER_MAX_NODES);
 
   derecho::SubgroupInfo si{derecho::DefaultSubgroupAllocator(
       {{std::type_index(typeid(sospdemo::FunctionTier)),
-        derecho::one_subgroup_policy(derecho::flexible_even_shards(
-            1, // number of shards in function tier subgroup
-            static_cast<int>(
-                function_tier_min_nodes), // minimum number of nodes in the
-                                          // single shard of function tier
-                                          // subgroup
-            static_cast<int>(
-                function_tier_max_nodes), // maximum number of nodes in the
-                                          // single shard of function tier
-                                          // subgroup
-            "FUNCTION_TIER"))},
+        derecho::one_subgroup_policy(derecho::flexible_even_shards("FUNCTION_TIER"))},
        {std::type_index(typeid(sospdemo::CategorizerTier)),
-        derecho::one_subgroup_policy(derecho::flexible_even_shards(
-            static_cast<int>(
-                categorizer_tier_num_shard), // number of shards in categorizer
-                                             // tier subgroup
-            static_cast<int>(
-                categorizer_tier_min_nodes), // minimum number of nodes in the
-                                             // single shard of categorizer tier
-                                             // subgroup
-            static_cast<int>(
-                categorizer_tier_max_nodes), // minimum number of nodes in the
-                                             // single shard of categorizer tier
-                                             // subgroup
-            "CATEGORIZER_TIER"))}})};
+        derecho::one_subgroup_policy(derecho::flexible_even_shards("CATEGORIZER_TIER"))}})};
 
   // 2 - prepare factories
   auto function_tier_factory = [](persistent::PersistentRegistry *) {
